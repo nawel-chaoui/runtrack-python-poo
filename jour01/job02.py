0431c09a4e70491dbd68c562cec2c8835b92735d
Aka Nawelfe <nawel.chaoui@laplateforme.io>
2023-04-24 10:55:12 +0200

    ajout job02

---
+++ jour01/job02.py
@@ -0,0 +1,8 @@
+class operation:
+    def __init__(self, nombre1=4, nombre2=3):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+
+operation = operation()
+print(operation.nombre1)
+print(operation.nombre2)
